df5e833edfb52f852e00274f75d3fdf5d7fbfb03
SamuelTribeUK <Samueltribeuk@outlook.com>
2020-09-19 09:23:25 +0100

    Implemented side bar with stop-start button and status and iterations indicators

--- gameOfLife.js
+++ gameOfLife.js
@@ -2,34 +2,33 @@
  * The game board is defined as a 2D array of objects which contain a mesh (called box) and a status (0 or 1 for dead or
  * alive)
  * TODO Implement a check for when nothing is moving after 2 turns then end the simulation
- * TODO Implement an iterations counter
  */
 
 let xSize = 20;
 let ySize = 20;
 
+let previousBoard;
+let gameBoard;
+
+let iterations = 0;
+let status = "stopped";
+
+let interval;
+
+document.getElementById("stopStart").innerText = "Start";
+
 let timeout = 200;
 
 let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth/(window.innerHeight - 50), 0.1, 1000);
 let renderer = new THREE.WebGLRenderer({antialias: true});
 let geometry = new THREE.BoxGeometry(0.8, 0.8, 0.8);
 let light = new THREE.PointLight(0xFFFFFF, 1, 500);
 
-let gameBoard;
-
 let addLights = function() {
 	let biggestSize = (xSize > ySize) ? xSize : ySize;
-	light.position.set(0, 0, biggestSize);
+	light.position.set(xSize/2, ySize/2, biggestSize);
 	scene.add(light);
-	// light.position.set(xSize, 0, biggestSize);
-	// scene.add(light);
-	// light.position.set(xSize, ySize, biggestSize);
-	// scene.add(light);
-	// light.position.set(0, ySize, biggestSize);
-	// scene.add(light);
-	// light.position.set(0, 0, biggestSize);
-	// scene.add(light);
 }
 
 let setupScene = function() {
@@ -40,13 +39,13 @@ let setupScene = function() {
 	}
 
 	renderer.setClearColor("#ffffff");
-	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setSize(window.innerWidth, window.innerHeight - 50);
 
 	document.body.appendChild(renderer.domElement);
 
 	window.addEventListener("resize", () => {
-		renderer.setSize(window.innerWidth, window.innerHeight);
-		camera.aspect = window.innerWidth / window.innerHeight;
+		renderer.setSize(window.innerWidth, window.innerHeight - 50);
+		camera.aspect = window.innerWidth / (window.innerHeight - 50);
 
 		camera.updateProjectionMatrix();
 	});
@@ -66,22 +65,16 @@ let initialiseBoard = function() {
 		gameBoard[i] = new Array(ySize);
 	}
 
-	// console.log(gameBoard);
-
 	for (let i = 0; i < xSize; i++) {
 		for (let j = 0; j < ySize; j++) {
 			let state = Math.floor(Math.random() * 2);
 			addMesh(state, i, j);
 		}
 	}
-
-	// console.log(gameBoard);
 }
 
 let simulateStep = function() {
 	let newGameBoard = $.extend(true, [], gameBoard);
-	// console.log(gameBoard);
-	// console.log(newGameBoard);
 
 	for (let i = 0; i < xSize; i++) {
 		for (let j = 0; j < ySize; j++) {
@@ -93,8 +86,6 @@ let simulateStep = function() {
 					}
 				}
 			}
-			// console.log("x: " + i + " y: " + j + " liveNum: " + liveNum);
-			// console.log(liveNum);
 			if (liveNum < 2) {
 				newGameBoard[i][j].state = 0;
 			} else if (liveNum < 4 && gameBoard[i][j].state === 1) {
@@ -102,15 +93,13 @@ let simulateStep = function() {
 			} else if (liveNum > 3 && gameBoard[i][j].state === 1) {
 				newGameBoard[i][j].state = 0;
 			} else if (liveNum === 3 && gameBoard[i][j].state === 0) {
-				// console.log("x: " + i + " y: " + j + " just went from 0 to 1");
 				newGameBoard[i][j].state = 1;
 			}
 		}
 	}
-	//console.log(newGameBoard);
 	gameBoard = $.extend(true, [], newGameBoard);
-	//console.log(gameBoard);
-
+	iterations += 1;
+	updateSidebar();
 	redraw();
 }
 
@@ -128,6 +117,10 @@ let addMesh = function(state, i, j) {
 	scene.add(gameBoard[i][j].box);
 }
 
+let updateSidebar = function() {
+	document.getElementById("status").innerText = "Status: " + status;
+	document.getElementById("iterations").innerText = "Iterations: " + iterations;
+}
 
 let checkCell = function(currX, currY) {
 	if (currX < 0 || currX >= ySize) {
@@ -137,13 +130,11 @@ let checkCell = function(currX, currY) {
 	} else if (gameBoard[currX][currY].state === 1) {
 		return 1;
 	} else {
-		//console.log(newGameBoard[currY][currX].state);
 		return 0;
 	}
 }
 
 let redraw = function() {
-	// scene.remove.apply(scene, scene.children);
 	let state;
 	let colour;
 	for (let i = 0; i < xSize; i++) {
@@ -155,8 +146,21 @@ let redraw = function() {
 				colour = "#19A74A";
 			}
 			gameBoard[i][j].box.material.color.set(colour);
-			// addLights();
 		}
+	}
+}
+
+let stopStart = function() {
+	if (status === "stopped") {
+		interval = setInterval(simulateStep, timeout);
+		document.getElementById("stopStart").innerText = "Stop";
+		status = "playing";
+		updateSidebar();
+	} else {
+		clearInterval(interval);
+		document.getElementById("stopStart").innerText = "Start";
+		status = "stopped";
+		updateSidebar();
 	}
 }
 
@@ -165,7 +169,6 @@ initialiseBoard(xSize, ySize);
 camera.position.x = xSize / 2;
 camera.position.y = ySize / 2;
 
-setInterval(simulateStep, timeout);
-// simulateStep();
+updateSidebar();
 
 render();
